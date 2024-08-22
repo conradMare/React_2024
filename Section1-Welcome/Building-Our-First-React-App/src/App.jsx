0fc958cbd3521ee5f3132a7e7a2487c0b54d9c2b
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-22 09:13:39 +0200

    2024/08/22(am)
    S01.02 - Building Our First React App! (Advice App)

--- Section1-Welcome/Building-Our-First-React-App/src/App.jsx
+++ Section1-Welcome/Building-Our-First-React-App/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Message from "./components/Message";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    // setCount(count + 1);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function() {
+    getAdvice();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message 
+        count={count}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
