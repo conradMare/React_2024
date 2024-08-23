e07058b3240fb6ec17859f85006309ba74bf54fb
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-23 05:47:11 +0200

    2024/08/23(am)
    Advice App - Updated

--- Section1-Welcome/Building-Our-First-React-App/src/main.jsx
+++ Section1-Welcome/Building-Our-First-React-App/src/main.jsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
